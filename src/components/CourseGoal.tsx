b1bf90f41a03bf28b50e2e28f2588f527736525d
Jose Nery <josenerydev@gmail.com>
2024-02-18 13:07:59 -0300

    initial

---
+++ src/components/CourseGoal.tsx
@@ -0,0 +1,24 @@
+import { type PropsWithChildren } from "react";
+
+type CourseGoalProps = PropsWithChildren<{
+  id: number;
+  title: string;
+  onDelete: (id: number) => void;
+}>;
+
+export default function CourseGoal({
+  title,
+  id,
+  children,
+  onDelete,
+}: CourseGoalProps) {
+  return (
+    <article>
+      <div>
+        <h2>{title}</h2>
+        {children}
+      </div>
+      <button onClick={() => onDelete(id)}>Delete</button>
+    </article>
+  );
+}
